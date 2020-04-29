9c91f3979458aba9b211c7bd3cd8a6dabb3b6559
Caio Costa <caiocosta12657@gmail.com>
2020-04-29 19:08:55 -0300

    Tela simples de cadastro

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -19,6 +20,19 @@ namespace SalesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]//Indicar que essa ação é de POST e não de GET
+        [ValidateAntiForgeryToken]//Previne ataques CSRF - Aproveita a autenticação para burlar o sistema
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
